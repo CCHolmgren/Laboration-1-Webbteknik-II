9e187b27678cd443e20f44fa12fbd5f90d1cf47f
Christfoffer Holmgren <ch222kv@student.lnu.se>
2014-11-11 11:23:10 +0100

    Doing a lot of changes. It should scrape a bit faster, and it scrapes after the user has initiated the page, which the script closes and says to the browser that there isn't anything left to send. So the user does not see a loading page for a lot of time, which is nice. Changed to file_get_contents instead of curl, shouldn't change that much, but it seemed to have a tad better performance after some testing, but it might not be that much of a difference now. It all depends on the server that it runs on and the server that it communicates with.

--- index.php
+++ index.php
@@ -5,53 +5,154 @@
  * Date: 2014-11-05
  * Time: 14:18
  */
-set_time_limit(100);
+const TIME_BETWEEN_SCRAPES = 600;
+const TIME_LIMIT = 300;
+set_time_limit(TIME_LIMIT);
 /*
  * Handles redirections and now I do not need to write all the ugly C-style initialization of curl
  */
-try{
-    if(file_exists("result.json")){
-        $previous_result_file = file_get_contents("result.json");
-        $previous_result = json_decode($previous_result_file, true);
-        if($previous_result["donewhen"]+600>time()){
-            echo "Minimum time until next scrape " . 600-(time() - $previous_result["donewhen"]);
-            var_dump($previous_result);
-            exit;
-        }
-    }
-}catch(Exception $e){
-}
-
-function curl($url, $user_agent, $retry = 0) {
+function curl($url, $retry = 0) {
+    $ch = curl_init();
+    $user_agent = "PHP cURL scraping Webbteknik II - Laboration 1 - ch222kv";
+    curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
     if ($retry > 5) {
         print "Maximum 5 retries are done, skipping!\n";
 
         return "in loop!";
     }
+    if ($ch === null) {
+        $ch = curl_init();
+    }
 
-    $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //curl_setopt($ch, CURLOPT_REFERER, 'http://www.google.com/');
-    curl_setopt($ch, CURLOPT_COOKIEFILE, "./cookie.txt");
-    curl_setopt($ch, CURLOPT_COOKIEJAR, "./cookie.txt");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
-    curl_close($ch);
+
 
     // handling the follow redirect
     if (preg_match("|Location: (https?://\S+)|", $result, $m)) {
         print "Manually doing follow redirect!\n$m[1]\n";
 
-        return curl($m[1], $user_agent, $retry + 1);
+        return curl($m[1], $user_agent, $retry + 1, $ch);
     }
 
     // add another condition here if the location is like Location: /home/products/index.php
-
     return $result;
 }
+
+function get_xpath($url) {
+    $page = get_page($url);
+
+    $domFirstPage = new DOMDocument();
+
+    //Because fuck HTML 5, right?
+    libxml_use_internal_errors(true);
+    $domFirstPage->loadHTML($page);
+    libxml_use_internal_errors(false);
+
+    $xpath = new DOMXPath($domFirstPage);
+
+    return $xpath;
+}
+
+function get_page($url) {
+    $opts = [
+        'http' => [
+            'method'     => 'GET',
+            'user_agent' => 'PHP cURL scraping Webbteknik II - Laboration 1 - ch222kv'
+        ]
+    ];
+    $context = stream_context_create($opts);
+
+    return mb_convert_encoding(file_get_contents($url, null, $context), 'HTML-ENTITIES', "UTF-8");
+}
+function scrape_courseList($url, $scrapeListPageArray, $pageBase, &$coursePageLinks, &$courseNames) {
+    if ($url === "") {
+        return;
+    }
+    global $scrapeCoursePageArray;
+    global $courses;
+    $nextPageLink = "";
+
+    $xpath = get_xpath($url);
+    foreach ($scrapeListPageArray as $xpath_name => $xpath_string) {
+        if ($xpath_name === "currentPath") {
+            continue;
+        } else if ($xpath_name === "nextPage") {
+            foreach ($xpath->query($xpath_string) as $x) {
+                $nextPageLink = $pageBase . $x->nodeValue;
+
+                if ($nextPageLink === $url) {
+                    return;
+                }
+            }
+        } else if ($xpath_name === "courseLinks") {
+            foreach ($xpath->query($xpath_string) as $x) {
+                //Exclude the links that aren't courses
+                if (strpos($x->nodeValue, $pageBase . "/kurs/") !== false) {
+                    //Recursive scraping the courses
+                    $courses["courses"][] = scrape_coursePage($x->nodeValue, $scrapeCoursePageArray);
+                }
+            }
+        }
+    }
+    if ($url === $nextPageLink) {
+        return;
+    }
+    scrape_courseList($nextPageLink, $scrapeListPageArray, $pageBase, $coursePageLinks, $courseNames);
+}
+
+
+function scrape_coursePage($url, $scrapeCoursePageArray) {
+    $object = [];
+    $object["courseURL"] = $url;
+    /*$page = get_page($url);
+    $domFirstPage = new DOMDocument();
+
+    //Because fuck HTML 5, right?
+    libxml_use_internal_errors(true);
+    $domFirstPage->loadHTML($page);
+    libxml_use_internal_errors(false);
+
+    $xpath = new DOMXPath($domFirstPage);*/
+    $xpath = get_xpath($url);
+
+    foreach ($scrapeCoursePageArray as $xpath_name => $xpath_string) {
+        foreach ($xpath->query($xpath_string) as $x) {
+            $object[$xpath_name] = trim($x->nodeValue);
+        }
+    }
+
+    return $object;
+}
+
+$timestarted = time();
+
+ob_end_clean();
+ob_start();
+header('Content-type: text/html; charset=utf-8');
+
+echo "<!DOCTYPE html><html><head><meta charset='UTF-8'> <title>Scrapy scrapy</title></head><body>";
+
+
+echo "Time that this page was generated: " . date("Y-m-d h:i:s", $timestarted) . "<br>";
+
+if (file_exists("result.json")) {
+    $previous_result_file = file_get_contents("result.json");
+    $previous_result = json_decode($previous_result_file, true);
+    if ($previous_result["donewhen"] + TIME_BETWEEN_SCRAPES > time()) {
+        $timenow = time();
+        echo "Minimum time until next scrape: " . (TIME_BETWEEN_SCRAPES - ($timenow - $previous_result["donewhen"])) . " seconds.";
+        var_dump($previous_result);
+        exit;
+    }
+} else if (file_exists("started_scraping.txt")) {
+    echo "The scraping has already started. It might take a while, please return later to find out when it is loaded";
+    exit;
+}
+
 
 /*
  * Methodology:
@@ -76,14 +177,14 @@ const COURSE_HEADING_XPATH = "//article[2]/header/h1/a/text()";
 const COURSE_AUTHOR_XPATH = "//article[2]/header/p/strong/text()";
 const COURSE_PUBLISHED_XPATH = "//article[2]/header/p/text()[1]";
 
-$scrapeCoursePageArray = ["courseName"=>COURSE_NAME_XPATH,
+$scrapeCoursePageArray = ["courseName"      => COURSE_NAME_XPATH,
                           //"courseURL"=>COURSE_URL_XPATH,
-                          "courseCode"=>COURSE_CODE_XPATH,
-                          "coursePlan"=>COURSE_PLAN_XPATH,
-                          "courseFirst"=>COURSE_FIRST_XPATH,
-                          "courseHeading"=>COURSE_HEADING_XPATH,
-                          "courseAuthor"=>COURSE_AUTHOR_XPATH,
-                          "coursePublished"=>COURSE_PUBLISHED_XPATH
+                          "courseCode"      => COURSE_CODE_XPATH,
+                          "coursePlan"      => COURSE_PLAN_XPATH,
+                          "courseFirst"     => COURSE_FIRST_XPATH,
+                          "courseHeading"   => COURSE_HEADING_XPATH,
+                          "courseAuthor"    => COURSE_AUTHOR_XPATH,
+                          "coursePublished" => COURSE_PUBLISHED_XPATH
 ];
 
 /*
@@ -102,172 +203,34 @@ $scrapeListPageArray = ["currentPath" => PAGE_CURRENT_XPATH, //Current page numb
 ];
 
 
-echo "<!DOCTYPE html><html><body>";
-
-$start_path = "https://coursepress.lnu.se/kurser/";
-$pageBase = "https://coursepress.lnu.se";
+$start_path = "http://coursepress.lnu.se/kurser/";
+$pageBase = "http://coursepress.lnu.se";
 $nextPageLink = "";
-$coursePageLinks = [];
-$courseNames = [];
 $times = 0;
 $stop = false;
 
-/*do {
-    $page = curl($start_path, "PHP cURL scraping Webbteknik II - Laboration 1 - ch222kv");
-
-    $domFirstPage = new DOMDocument();
-
-    //Because fuck HTML 5, right?
-    libxml_use_internal_errors(true);
-    $domFirstPage->loadHTML($page);
-    libxml_use_internal_errors(false);
-
-    $xpath = new DOMXPath($domFirstPage);
-
-    foreach ($scrapeListPageArray as $xpath_name => $xpath_string) {
-        if ($xpath_name === "currentPath") {
-            continue;
-        } else if ($xpath_name === "nextPage") {
-            foreach ($xpath->query($xpath_string) as $x) {
-                $nextPageLink = $pageBase . $x->nodeValue;
-                echo $nextPageLink . "<br>" . $start_path . "<br>";
-
-                if ($nextPageLink === $start_path) {
-                    $stop = true;
-                }
-            }
-        } else if ($xpath_name === "courseLinks") {
-            foreach ($xpath->query($xpath_string) as $x) {
-                //Exclude the links that aren't courses
-                if(strpos($x->nodeValue, $pageBase."/kurs/") !== false){
-                    $coursePageLinks[] = $x->nodeValue;
-                }
-            }
-        } else if ($xpath_name === "courseNames") {
-            foreach ($xpath->query($xpath_string) as $x) {
-                $courseNames[] = $x->nodeValue;
-            }
-        }
-    }
-    if($start_path === $nextPageLink)
-        break;
-    $start_path = $nextPageLink;
-} while (true);*/
 $coursePageLinks = [];
 $courseNames = [];
-scrape_courseList("https://coursepress.lnu.se/kurser/", $scrapeListPageArray, $pageBase, $coursePageLinks, $courseNames);
+$courses = ["courses"=>[]];
 
-function scrape_courseList($url, $scrapeListPageArray, $pageBase, &$coursePageLinks, &$courseNames){
-    if($url==="")
-        return;
-    $page = curl($url, "PHP cURL scraping Webbteknik II - Laboration 1 - ch222kv");
-    $nextPageLink = "";
-    $domFirstPage = new DOMDocument();
+header("Connection: close");
 
+echo "Starting the scraping of course list";
+file_put_contents("started_scraping.txt", "");
 
-    //Because fuck HTML 5, right?
-    libxml_use_internal_errors(true);
-    $domFirstPage->loadHTML($page);
-    libxml_use_internal_errors(false);
-
-    $xpath = new DOMXPath($domFirstPage);
-    foreach ($scrapeListPageArray as $xpath_name => $xpath_string) {
-        if ($xpath_name === "currentPath") {
-            continue;
-        } else if ($xpath_name === "nextPage") {
-            foreach ($xpath->query($xpath_string) as $x) {
-                $nextPageLink = $pageBase . $x->nodeValue;
-                echo $nextPageLink . "<br>" . $url . "<br>";
-
-                if ($nextPageLink === $url) {
-                    $stop = true;
-                }
-            }
-        } else if ($xpath_name === "courseLinks") {
-            foreach ($xpath->query($xpath_string) as $x) {
-                //Exclude the links that aren't courses
-                if(strpos($x->nodeValue, $pageBase."/kurs/") !== false){
-                    $coursePageLinks[] = $x->nodeValue;
-                }
-            }
-        }
-        /*else if ($xpath_name === "courseNames") {
-            foreach ($xpath->query($xpath_string) as $x) {
-                $courseNames[] = $x->nodeValue;
-            }
-        }*/
-    }
-    if($url === $nextPageLink)
-        return;
-
-    scrape_courseList($nextPageLink, $scrapeListPageArray, $pageBase, $coursePageLinks, $courseNames);
-}
-/*foreach($courseNames as $cn){
-    //echo $cn . "<br>";
-}*/
-foreach ($coursePageLinks as $cpl) {
-    echo $cpl . "<br>";
-}
-$courses = [];
-$courses["courses"]=[];
-foreach($coursePageLinks as $cpl){
-    $courses["courses"][] = scrape_coursePage($cpl, $scrapeCoursePageArray);
-
-}
-$courses["donewhen"] = time();
-$courses["amount_of_courses"] = count($courses["courses"])-1;
-file_put_contents("result.json",json_encode($courses));
-
-function scrape_coursePage($url, $scrapeCoursePageArray){
-    $object = [];
-    $object["courseURL"] = $url;
-
-    $page = curl($url, "PHP cURL scraping Webbteknik II - Laboration 1 - ch222kv");
-    $domFirstPage = new DOMDocument();
-
-    //Because fuck HTML 5, right?
-    libxml_use_internal_errors(true);
-    $domFirstPage->loadHTML($page);
-    libxml_use_internal_errors(false);
-
-    $xpath = new DOMXPath($domFirstPage);
-
-    foreach ($scrapeCoursePageArray as $xpath_name => $xpath_string) {
-        foreach($xpath->query($xpath_string) as $x){
-            $object[$xpath_name] = trim($x->nodeValue);
-        }
-    }
-    /*foreach($scrapeCoursePageArray as $xpath_name=>$xpath_string){
-        if(!isset($object[$xpath_name])){
-            $object[$xpath_name] = "no information";
-        }
-    }*/
-    return $object;
-}
-
-
-
-
-
-
-
-/*
-$response = curl("https://coursepress.lnu.se/kurs/brukarorienterad-design/", "PHP cURL testing ch222kv");
-
-$dom = new DOMDocument();
-
-//Because fuck HTML 5, right?
-libxml_use_internal_errors(true);
-$dom->loadHTML($response);
-libxml_use_internal_errors(false);
-$dom->preserveWhiteSpace = false;
-$xpath = new DOMXPath($dom);
-
-foreach ($scrapeCoursePageArray as $xpath_string) {
-    foreach ($xpath->query($xpath_string) as $x) {
-        echo "<pre>";
-        var_export(trim($x->nodeValue));
-        echo "</pre><br>";
-    }
-}*/
 echo "</body></html>";
+$size = ob_get_length();
+
+header("Content-Length: $size");
+ob_end_flush();
+flush();
+session_write_close();
+
+scrape_courseList($start_path, $scrapeListPageArray, $pageBase, $coursePageLinks, $courseNames);
+$courses["donewhen"] = time();
+$courses["timestarted"] = $timestarted;
+$courses["timetaken"] = $courses["donewhen"] - $timestarted;
+$courses["amount_of_courses"] = count($courses["courses"]) - 1;
+
+file_put_contents("result.json", json_encode($courses));
+unlink("started_scraping.txt");
